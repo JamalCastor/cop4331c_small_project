1e405bbe221a3bcbeb9e1294df7ad0a4ac66a28e
Jamal Castor <jxmalbusiness@gmail.com>
2026-05-19 19:07:19 -0400

    Updating API (local testing not remote testing)

--- .vscode/Source code/API/Login.php
+++ .vscode/Source code/API/Login.php
@@ -7,15 +7,16 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	$conn = new mysqli("localhost", "root", "", "ContactManager"); 	 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
+		$password = md5($inData["password"]);
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("ss", $inData["login"], $password);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
