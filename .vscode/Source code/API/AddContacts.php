ddd9731fae8595b84e1484e9b1edaab03a8bddba
Jamal Castor <jxmalbusiness@gmail.com>
2026-05-20 22:29:11 -0400

    Fixing add contact php

--- .vscode/Source code/API/AddContacts.php
+++ .vscode/Source code/API/AddContacts.php
@@ -1,25 +1,32 @@
 <?php
 	$inData = getRequestInfo();
-	
-	$contact = $inData["contact"];
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
 	$userId = $inData["userId"];
 
 	$conn = new mysqli("localhost", "TheBeast", "COP4331//x26SP", "ContactManager");
-	if ($conn->connect_error) 
+
+	if ($conn->connect_error)
 	{
-		returnWithError( $conn->connect_error );
-	} 
+		returnWithError($conn->connect_error);
+	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $contact);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Phone,Email,UserID) VALUES(?,?,?,?,?)");
+
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
+
 		$stmt->execute();
 
 		$newId = $conn->insert_id;
 
 		$stmt->close();
 		$conn->close();
-		returnWithError("", $newId);
+
+		returnWithInfo($newId);
 	}
 
 	function getRequestInfo()
@@ -27,16 +34,21 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
-	function returnWithError( $err, $newId = -1 )
+
+	function returnWithError($err)
 	{
-		$retValue = '{"newId":' . $newId . ',"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		$retValue = '{"newId":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
 	}
-	
+
+	function returnWithInfo($newId)
+	{
+		$retValue = '{"newId":' . $newId . ',"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
 ?>
